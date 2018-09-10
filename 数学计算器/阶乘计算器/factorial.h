6d762265a0ad716f22f9b0fe3bcf7929fd6b75a1
theblackworld <33271035+theblackworld@users.noreply.github.com>
2018-09-10 19:06:31 +0800

    Update factorial.h

--- 数学计算器/阶乘计算器/factorial.h
+++ 数学计算器/阶乘计算器/factorial.h
@@ -3,35 +3,36 @@ using std::vector;
 
 class Multiply{
    public:
+     
      Multiply():num_(0),result_(-1),pos_(-1){}
 
-	 Multiply(int num):num_(num),result_(-1),pos_(-1){}
+     Multiply(int num):num_(num),result_(-1),pos_(-1){}
+ 
+     void Set(int num){num_ = num;}
 
-	 void Set(int num){num_ = num;}
-
-	 void Calculate();  //to store all the num in the result_ or the result_vec;
+     void Calculate();  //to store all the num in the result_ or the result_vec;
 
      long long Count(int n);  //if (num_ < 20) no need to allocate more space;
 
-	 int SumByVector(vector<int>& );
+     int SumByVector(vector<int>& );
+ 
+     int SumByNum(long long num);
 
-	 int SumByNum(long long num);
-
-	 void ShowResult();
+     void ShowResult();
 	 
-	 int CountEach();   //计算每一位数的相等和;
+     int CountEach();   //计算每一位数的相等和;
 
    private:
      //no copying allowed;
      Multiply(const Multiply&);
 
-	 Multiply& operator=(const Multiply&);
+     Multiply& operator=(const Multiply&);
      // num!
-	 int num_;  
+     int num_;  
 	 // 当阶乘小于20的时候此位有效，其余置为-1;
      long long result_;
      // 当前数字的位数,当且仅当数字极大阶乘超过20的时候此成员有效
-	 int pos_;  
-	 // 存储大数
-	 vector<int> result_vec_;
+     int pos_;  
+	 // 存储大数 
+     vector<int> result_vec_;
 };
